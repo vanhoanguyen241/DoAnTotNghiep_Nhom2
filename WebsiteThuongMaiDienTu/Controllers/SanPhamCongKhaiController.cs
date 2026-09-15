06d8af65a3aad81122d86b6abb3bc42471ab3725
Hoà <vanhoanguyen241@gmail.com>
2026-09-15 10:29:16 +0700

    Loại bỏ partial views, làm đẹp dropdown sản phẩm

--- WebsiteThuongMaiDienTu/Controllers/SanPhamCongKhaiController.cs
+++ WebsiteThuongMaiDienTu/Controllers/SanPhamCongKhaiController.cs
@@ -14,16 +14,13 @@ namespace WebsiteThuongMaiDienTu.Controllers
         {
             int pageSize = 12;
             int pageNumber = page ?? 1;
-
             var query = db.sanphams.AsQueryable();
 
             // Lọc theo loại sản phẩm
             if (!string.IsNullOrEmpty(maloai))
             {
                 query = query.Where(sp => sp.maloaisanpham == maloai);
-
                 var loai = db.loaisanphams.FirstOrDefault(l => l.maloaisanpham == maloai);
-
                 ViewBag.MaLoai = maloai;
                 ViewBag.TenLoai = loai != null ? loai.tenloaisanpham : "";
             }
@@ -34,7 +31,6 @@ namespace WebsiteThuongMaiDienTu.Controllers
                 query = query.Where(sp =>
                     sp.tensanpham.Contains(tukhoa) ||
                     sp.mota.Contains(tukhoa));
-
                 ViewBag.TuKhoa = tukhoa;
             }
 
@@ -44,15 +40,9 @@ namespace WebsiteThuongMaiDienTu.Controllers
             // Tính phân trang đơn giản
             int tongSoSanPham = query.Count();
             int tongSoTrang = (int)Math.Ceiling((double)tongSoSanPham / pageSize);
-
             ViewBag.TongSoSanPham = tongSoSanPham;
             ViewBag.TongSoTrang = tongSoTrang;
             ViewBag.TrangHienTai = pageNumber;
-
-            // Lấy danh sách loại để render bộ lọc
-            ViewBag.DanhSachLoai = db.loaisanphams
-                .OrderBy(l => l.tenloaisanpham)
-                .ToList();
 
             var danhSach = query
                 .Skip((pageNumber - 1) * pageSize)
@@ -65,27 +55,15 @@ namespace WebsiteThuongMaiDienTu.Controllers
         // GET: /SanPhamCongKhai/ChiTiet/SP01
         public ActionResult ChiTiet(string id)
         {
-            if (string.IsNullOrEmpty(id))
-            {
-                return HttpNotFound();
-            }
-
+            if (string.IsNullOrEmpty(id)) return HttpNotFound();
             var sanpham = db.sanphams.Find(id);
+            if (sanpham == null) return HttpNotFound();
 
-            if (sanpham == null)
-            {
-                return HttpNotFound();
-            }
-
-            var loai = db.loaisanphams
-                .FirstOrDefault(l => l.maloaisanpham == sanpham.maloaisanpham);
-
-            var nsx = db.donvisanxuats
-                .FirstOrDefault(d => d.madonvisanxuat == sanpham.madonvisanxuat);
+            var loai = db.loaisanphams.FirstOrDefault(l => l.maloaisanpham == sanpham.maloaisanpham);
+            var nsx = db.donvisanxuats.FirstOrDefault(d => d.madonvisanxuat == sanpham.madonvisanxuat);
 
             ViewBag.TenLoai = loai != null ? loai.tenloaisanpham : "";
             ViewBag.TenNSX = nsx != null ? nsx.tendonvisanxuat : "";
-
             return View(sanpham);
         }
     }
